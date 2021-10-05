5aacb3f289af1042237c1c968bef9f2790c5e6be
Ahmed Valyaev <45922762+4bselyam@users.noreply.github.com>
2021-10-05 21:32:05 +0300

    refactor

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-  $("select").formSelect();
+  $("select").formSelect({
+    classes: "select-el",
+  });
+});
+
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+$(".carousel.carousel-slider").carousel({
+  fullWidth: true,
+  indicators: true,
+});
+
+$(document).ready(function () {
+  $(".parallax").parallax();
+});
+
+$(document).ready(function () {
+  $(".fixed-action-btn").floatingActionButton();
+});
+
+$(document).ready(function () {
+  $(".modal").modal();
 });
